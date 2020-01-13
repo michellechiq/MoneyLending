51ec13b89bd430e6ce56fa5576ca1986a08272cf
Nadine Rodriguez <nadine.rodriguez@gmail.com>
2020-01-13 14:01:07 -0500

    multiple pages

--- App.js
+++ App.js
@@ -1,24 +1,81 @@
 import React from 'react';
-import { StyleSheet, Text, View,TextInput, Button } from 'react-native';
+import Example from './screens/Example';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return(<View style= {styles.container}>
-    <TextInput placeholder="Email" style={styles.textInput}/>
-    <TextInput placeholder="Password" style={styles.textInput}/>
-    <Button title="LOGIN" />
-  </View>);
-};
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 100,
-    justifyContent: 'space-between',
-    alignItems: 'center'
+const AuthStack = createStackNavigator({
+  Landing: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Landing',
+    },
   },
-  textInput: {
-    width: '100%',
-    borderBottomColor: 'green',
-    borderBottomWidth: 1,
-    padding:20
+  SignIn: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Sign In',
+    },
+  },
+  CreateAccount: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Create Account',
+    },
+  },
+  ForgotPassword: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Forgot Password',
+    },
+  },
+  ResetPassword: {
+    screen: Example,
+    navigationOptions: {
+      headerTitle: 'Reset Password',
+    },
   },
 });
+
+const App = createSwitchNavigator({
+  Loading: {
+    screen: Example,
+  },
+  Auth: {
+    screen: AuthStack,
+  },
+  App: {
+    screen: Example,
+  },
+});
+
+export default createAppContainer(App);
+
+// export default function App() {
+//   return(
+//
+//     <View style= {styles.container}>
+//     <Text>Create an Account</Text>
+//     <TextInput placeholder="Email" style={styles.textInput}/>
+//     <TextInput placeholder="Password" style={styles.textInput}/>
+//     <Button title="LOGIN" />
+//   </View>);
+// };
+//
+// const styles = StyleSheet.create({
+//   topPart: {
+//     padding:20,
+//   },
+//   container: {
+//     top: 250,
+//     padding: 50,
+//     justifyContent: 'space-between',
+//     alignItems: 'center'
+//   },
+//   textInput: {
+//     width: '100%',
+//     borderBottomColor: 'green',
+//     borderBottomWidth: 1,
+//     padding:20
+//   },
+// });

b5503c17f1412700290ea7327e74022e2a4b72de
Nadine Rodriguez <nadine.rodriguez@gmail.com>
2020-01-17 23:04:32 -0500

    added borrowing page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View, Text, TextInput, StyleSheet, Image} from 'react-native';
+import { Button, View, Text, TextInput, StyleSheet, Image, FlatList, AppRegistry, Platform} from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
@@ -106,20 +106,56 @@ class Lending extends React.Component {
   }
 }
 class Borrowing extends React.Component {
+  constructor(props)
+ {
+   super(props);
+
+   this.state = { GridViewItems: [
+     {key: 'One'},
+     {key: 'Two'},
+     {key: 'Three'},
+     {key: 'Four'},
+     {key: 'Five'},
+     {key: 'Six'},
+     {key: 'Seven'},
+     {key: 'Eight'},
+     {key: 'Nine'},
+     {key: 'Zero'}
+   ]}
+ }
+GetGridViewItem (item) {
+
+Alert.alert(item);
+
+}
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center',backgroundColor: '#E8FEEA' }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center',backgroundColor: '#A9FBD7' }}>
         <Text style = {{fontSize:22, bottom: 200}}>Borrow</Text>
-        <TextInput placeholder="Name of Bank" style={styles.textInput}/>
-        <TextInput placeholder="Account Number" style={styles.textInput}/>
-        <TextInput placeholder="9 Digit Routing Number" style={styles.textInput}/>
+        <TextInput placeholder="200" style={styles.textInput2}/>
+        <TextInput placeholder="Account Number" style={styles.textInput2}/>
         <Button
-          title="Switch To Lender"
-          onPress={() => this.props.navigation.navigate('Lender')}
+        title="Borrow as anonymous"
+        color=" #cccac9"
+        onPress={this.handlepress}
         />
-      </View>
+        <Text style= {{fontSize:20, bottom: 120}}> Interest Rate: </Text>
+        <TextInput placeholder="10%" style={styles.textInput3}/>
+        <TextInput placeholder="1" style={styles.textInput4}/>
+        <TextInput placeholder="2" style={styles.textInput5}/>
+        <TextInput placeholder="3" style={styles.textInput6}/>
+        <TextInput placeholder="4" style={styles.textInput7}/>
+        <TextInput placeholder="5" style={styles.textInput8}/>
+        <TextInput placeholder="6" style={styles.textInput9}/>
+        <TextInput placeholder="7" style={styles.textInput10}/>
+        <TextInput placeholder="9" style={styles.textInput11}/>
+        <TextInput placeholder="0" style={styles.textInput12}/>
+
+    </View>
 
     );
+  }
+  handlepress = () => {
   }
 }
 class ProfB extends React.Component {
@@ -371,7 +407,90 @@ const styles = StyleSheet.create({
     bottom:60,
     margin: 20,
     textAlign: 'right'
-  }
+  },
+  textInput2: {
+    width: '50%',
+    borderColor: '#54e2a5',
+    borderWidth: 5,
+    backgroundColor:'white',
+    fontSize: 25,
+    bottom:120,
+    padding:10,
+    margin: 15,
+    textAlign:'left'
+
+  },
+  textInput3: {
+    width: '25%',
+    borderColor: '#54e2a5',
+    borderWidth: 5,
+    fontSize: 10,
+    bottom:120,
+    padding:10,
+    margin: 15,
+    textAlign:'left'
+
+  },
+  textInput4:{
+    width: '15%',
+    borderColor: '#54e2a5',
+    borderWidth: 5,
+    fontSize: 10,
+    bottom:100,
+    right:75,
+    padding:10,
+    margin: 15,
+    borderRadius: 20,
+    textAlign:'left'
+  },
+  textInput5:{
+    width: '15%',
+    borderColor: '#54e2a5',
+    borderWidth: 5,
+    fontSize: 10,
+    bottom:172,
+    right:10,
+    padding:10,
+    margin: 15,
+    borderRadius: 20,
+    textAlign:'left'
+  },
+  textInput6:{
+    width: '15%',
+    borderColor: '#54e2a5',
+    borderWidth: 5,
+    fontSize: 10,
+    bottom:245,
+    left:60,
+    padding:10,
+    borderRadius: 20,
+    margin: 15,
+    textAlign:'left'
+  },
+  textInput7:{
+    width: '15%',
+    borderColor: '#54e2a5',
+    borderRadius: 20,
+    borderWidth: 5,
+    fontSize: 10,
+    bottom:265,
+    left:60,
+    padding:10,
+    margin: 15,
+    textAlign:'left'
+  },
+  textInput8:{
+    width: '15%',
+    borderColor: '#54e2a5',
+    borderRadius: 20,
+    borderWidth: 5,
+    fontSize: 10,
+    top:150,
+    left:50,
+    padding:10,
+    margin: 15,
+    textAlign:'left'
+  },
 });
 
 
